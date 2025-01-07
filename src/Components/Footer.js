c6e0e84a85dea0acdab572db174bee677f2403d0
aniketwable <aniketwable57@gmail.com>
2025-01-07 19:40:15 +0530

    ??? Second ???

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,18 +1,32 @@
 import React from "react";
-import "./CSS/Footer.css";
+
 
 function Footer() {
   return (
-    <footer className="footer">
-      <div className="footer-content">
-        <p>&copy; 2025 Futuristic Website. All rights reserved.</p>
-        <div className="social-icons">
-          <a href="#facebook" className="icon">FB</a>
-          <a href="#twitter" className="icon">TW</a>
-          <a href="#linkedin" className="icon">LI</a>
-        </div>
-      </div>
-    </footer>
+<footer>
+  <div className="row">
+    <div className="col-six tab-full pull-right social">
+      <ul className="footer-social">        
+        <li><a href="#"><i className="fa fa-facebook" /></a></li>
+        <li><a href="#"><i className="fa fa-behance" /></a></li>
+        <li><a href="#"><i className="fa fa-twitter" /></a></li>
+        <li><a href="#"><i className="fa fa-dribbble" /></a></li>
+        <li><a href="#"><i className="fa fa-instagram" /></a></li>
+      </ul> 
+    </div>
+    <div className="col-eight tab-full">
+      <div className="copyright">
+        <span>© Copyright 2018 </span> 
+        <span>Design by <a href="http://www.styleshout.com/">styleshout</a></span> 
+        <span>Distributed by <a href="https://themewagon.com/">themewagon</a></span>	         	
+      </div>		                  
+    </div>
+    <div id="go-top">
+      <a className="smoothscroll" title="Back to Top" href="#top"><i className="fa fa-long-arrow-up" /></a>
+    </div>
+  </div>    	
+</footer>
+ 
   );
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,31 @@
 import React from "react";
-import "./CSS/Header.css";
+
 
 function Header() {
   return (
-    <header className="header">
-      <div className="logo">FuturisticLogo</div>
-      <nav className="nav">
-        <a href="#home">Home</a>
-        <a href="#about">About</a>
-        <a href="#services">Services</a>
-        <a href="#contact">Contact</a>
-      </nav>
-    </header>
+<header>   	
+  <div className="row">
+    <div className="top-bar">
+      <a className="menu-toggle" href="#"><span>Menu</span></a>
+      <div className="logo">
+        <a href="index.html">KARDS</a>
+      </div>		      
+      <nav id="main-nav-wrap">
+        <ul className="main-navigation">
+          <li className="current"><a className="smoothscroll" href="#intro" title>Home</a></li>
+          <li><a className="smoothscroll" href="#about" title>About</a></li>
+          <li><a className="smoothscroll" href="#resume" title>Resume</a></li>
+          <li><a className="smoothscroll" href="#portfolio" title>Portfolio</a></li>
+          <li><a className="smoothscroll" href="#services" title>Services</a></li>					
+          <li><a className="smoothscroll" href="#contact" title>Contact</a></li>	
+          <li><a href="styles.html" title>Style Demo</a></li>				
+        </ul>
+      </nav>    		
+    </div> 
+  </div> 	
+</header>
+
+
   );
 }
 
